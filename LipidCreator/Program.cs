15963dc5e6b3703a6d75606a7e549451deb037b8
dominik.kopczynski <dominik.kopczynski@isas.de>
2016-12-11 20:10:30 +0100

    added newline

--- LipidCreator/Program.cs
+++ LipidCreator/Program.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.IO;
+
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;

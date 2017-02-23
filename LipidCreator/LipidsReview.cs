a40f973c5c0267fded876a36dcb6c7a7b9f10a2c
dominik.kopczynski <dominik.kopczynski@isas.de>
2017-02-23 14:34:00 +0100

    storeing transition list implemented

--- LipidCreator/LipidsReview.cs
+++ LipidCreator/LipidsReview.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-//using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace LipidCreator
@@ -15,6 +15,7 @@ namespace LipidCreator
     {
         public DataTable allFragments;
         public DataTable allFragmentsUnique;
+        public DataTable currentView;
         public LipidCreatorForm lipidCreatorForm;
         public LipidsReview(LipidCreatorForm lipidCreatorForm, DataTable allFragments, DataTable allFragmentsUnique)
         {
@@ -22,7 +23,8 @@ namespace LipidCreator
             this.allFragments = allFragments;
             this.allFragmentsUnique = allFragmentsUnique;
             InitializeComponent();
-            dataGridView1.DataSource = this.allFragments;
+            currentView = this.allFragments;
+            dataGridView1.DataSource = currentView;
             foreach (DataGridViewColumn dgvc in dataGridView1.Columns)
             {
                 dgvc.SortMode = DataGridViewColumnSortMode.NotSortable;
@@ -41,19 +43,49 @@ namespace LipidCreator
         {
             if (((CheckBox)sender).Checked)
             {
-                dataGridView1.DataSource = this.allFragmentsUnique;
+                currentView = this.allFragmentsUnique;
             }
             else
             {
-                dataGridView1.DataSource = this.allFragments;
+                currentView = this.allFragments;
             }
+            dataGridView1.DataSource = currentView;
             dataGridView1.Update();
             dataGridView1.Refresh();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            
+            saveFileDialog1.InitialDirectory = "c:\\";
+            saveFileDialog1.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.RestoreDirectory = true;
 
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                StreamWriter writer;
+                if((writer = new StreamWriter(saveFileDialog1.OpenFile())) != null)
+                {
+                    foreach (DataRow row in currentView.Rows)
+                    {
+                        writer.WriteLine((String)row["Molecule List Name"] + "," +
+                        (String)row["Precursor Name"] + "," +
+                        (String)row["Precursor Ion Formula"] + "," +
+                        (String)row["Precursor Adduct"] + "," +
+                        ((String)row["Precursor m/z"]).Replace(",", ".") + "," +
+                        (String)row["Precursor Charge"] + "," +
+                        (String)row["Pruduct Name"] + "," +
+                        (String)row["Pruduct Ion Formula"] + "," +
+                        ((String)row["Pruduct m/z"]).Replace(",", ".") + "," +
+                        (String)row["Pruduct Charge"]);
+                    }
+                    writer.Dispose();
+                    writer.Close();
+                }
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)

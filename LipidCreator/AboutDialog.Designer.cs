b036c3c57ce1bea02e20fed8c4ab76419b11c61d
Nils Hoffmann <nils.hoffmann@isas.de>
2017-08-14 17:40:20 +0200

    [LC-11] Added first version of About Dialog.

---
+++ LipidCreator/AboutDialog.Designer.cs
@@ -0,0 +1,112 @@
+﻿/*
+MIT License
+
+Copyright (c) 2017 Dominik Kopczynski   -   dominik.kopczynski {at} isas.de
+                   Bing Peng   -   bing.peng {at} isas.de
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace LipidCreator
+{
+    partial class AboutDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.textLibraryName = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(29, 66);
+            this.buttonOK.Name = "button1";
+            this.buttonOK.Size = new System.Drawing.Size(99, 30);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOKClick);
+            // 
+            // label1
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Location = new System.Drawing.Point(12, 9);
+            this.linkLabel.Name = "label1";
+            this.linkLabel.Size = new System.Drawing.Size(221, 13);
+            this.linkLabel.TabIndex = 2;
+            this.linkLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.SystemDefault;
+            this.linkLabel.Text = "http://lifs.isas.de/lipidcreator";
+            this.linkLabel.LinkClicked += this.linkLabel_LinkClicked;
+            // 
+            // textBox1
+            // 
+            this.textLibraryName.Location = new System.Drawing.Point(15, 25);
+            this.textLibraryName.Name = "textBox1";
+            this.textLibraryName.Size = new System.Drawing.Size(400, 200);
+            this.textLibraryName.TabIndex = 1;
+            this.textLibraryName.Text = "Contributers: \r\nBing Peng\r\nDominik Kopzcyinski\r\nNils Hoffmann";
+            // 
+            // AboutDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 320);
+            this.Controls.Add(this.textLibraryName);
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.buttonOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "AboutDialog";
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.RichTextBox textLibraryName;
+    }
+}

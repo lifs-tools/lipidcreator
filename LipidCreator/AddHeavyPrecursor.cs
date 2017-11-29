7777d6fb787a85e16096847bbba4123a517d7633
dominik.kopczynski <dominik.kopczynski@isas.de>
2017-11-29 09:57:24 +0100

    adding heavy isotope form files

---
+++ LipidCreator/AddHeavyPrecursor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LipidCreator
+{
+    public partial class AddHeavyPrecursor : Form
+    {
+        public AddHeavyPrecursor(CreatorGUI creatorGUI, Lipid currentLipid)
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

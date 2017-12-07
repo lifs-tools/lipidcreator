3c5ae69d45bd77582d0d9a2b29a0eb547408f434
dominik.kopczynski <dominik.kopczynski@isas.de>
2017-12-07 09:23:59 +0100

    changed function of add heavy isotope in manage heavy isotopes

--- LipidCreator/AddHeavyPrecursor.Designer.cs
+++ LipidCreator/AddHeavyPrecursor.Designer.cs
@@ -163,9 +163,9 @@ namespace LipidCreator
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(203, 514);
+            this.button1.Location = new System.Drawing.Point(183, 514);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(95, 23);
             this.button1.TabIndex = 10;
             this.button1.Text = "Close Window";
             this.button1.UseVisualStyleBackColor = true;

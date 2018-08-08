c24fb4364f938cf3d6edc7164bcfa1a97e6a4a58
dominik.kopczynski <dominik.kopczynski@isas.de>
2018-08-08 13:39:25 +0200

    added pure datagridtable

--- LipidCreator/TranslatorDialog.Designer.cs
+++ LipidCreator/TranslatorDialog.Designer.cs
@@ -59,6 +59,10 @@ namespace LipidCreator
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Foo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -91,20 +95,44 @@ namespace LipidCreator
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Foo,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(774, 340);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Foo
+            // 
+            this.Foo.HeaderText = "Old lipid name";
+            this.Foo.Name = "Foo";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Current lipid name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
             // TranslatorDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(798, 393);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "TranslatorDialog";
-            this.Text = "Lipid names translation";
-            this.ResumeLayout(false);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+            this.Name = "TranslatorDialog";
+            this.Text = "Lipid names translation";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
 
         }
 
@@ -113,5 +141,8 @@ namespace LipidCreator
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Foo;
+        private DataGridViewTextBoxColumn Column1;
     }
 }

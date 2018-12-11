40c87dd9e301a30e03690b74318e2c09bf0b31a0
dominik.kopczynski <dominik.kopczynski@isas.de>
2018-12-11 14:22:30 +0100

    optimized merge/resize dialog

--- LipidCreator/LCMessageBox.Designer.cs
+++ LipidCreator/LCMessageBox.Designer.cs
@@ -44,32 +44,33 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(182, 77);
+            this.button1.Location = new System.Drawing.Point(139, 60);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Merge";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler (mergeClick);
+            this.button1.Click += new System.EventHandler(this.mergeClick);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(274, 77);
+            this.button2.Location = new System.Drawing.Point(220, 60);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Replace";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler (replaceClick);
+            this.button2.Click += new System.EventHandler(this.replaceClick);
             // 
             // LCMessageBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 112);
+            this.ClientSize = new System.Drawing.Size(310, 93);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LCMessageBox";

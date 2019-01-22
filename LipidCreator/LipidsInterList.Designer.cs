7ede6eb9aa10670c1bab4fa6258e5d94f87ca5b4
Dominik Kopczynski <dominik.kopczynski@isas.de>
2019-01-22 17:07:14 +0100

    adding new UI window for listing intermediate lipids

---
+++ LipidCreator/LipidsInterList.Designer.cs
@@ -0,0 +1,196 @@
+﻿/*
+MIT License
+
+Copyright (c) 2018 Dominik Kopczynski   -   dominik.kopczynski {at} isas.de
+                   Bing Peng   -   bing.peng {at} isas.de
+                   Nils Hoffmann  -  nils.hoffmann {at} isas.de
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+using System;
+
+namespace LipidCreator
+{
+    partial class LipidsInterList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewTransitions = new System.Windows.Forms.DataGridView();
+            this.buttonSendToSkyline = new System.Windows.Forms.Button();
+            this.buttonStoreTransitionList = new System.Windows.Forms.Button();
+            this.checkBoxHideReplicates = new System.Windows.Forms.CheckBox();
+            this.checkBoxCreateSpectralLibrary = new System.Windows.Forms.CheckBox();
+            this.buttonStoreSpectralLibrary = new System.Windows.Forms.Button();
+            this.labelNumberOfTransitions = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransitions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridViewTransitions.AllowUserToAddRows = false;
+            this.dataGridViewTransitions.AllowUserToDeleteRows = false;
+            //this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridViewTransitions.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewTransitions.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.None;
+            this.dataGridViewTransitions.AllowUserToResizeRows = false;
+            this.dataGridViewTransitions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTransitions.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewTransitions.Name = "dataGridView1";
+            this.dataGridViewTransitions.ReadOnly = true;
+            this.dataGridViewTransitions.RowHeadersVisible = false;
+            this.dataGridViewTransitions.RowTemplate.Height = 34;
+            this.dataGridViewTransitions.Size = new System.Drawing.Size(1055, 409);
+            this.dataGridViewTransitions.TabIndex = 0;
+            this.dataGridViewTransitions.DataBindingComplete += new DataGridViewBindingCompleteEventHandler(gridviewDataBindingComplete);
+            this.dataGridViewTransitions.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.EnableResizing; 
+            // 
+            // button1
+            // 
+            this.buttonSendToSkyline.Location = new System.Drawing.Point(410, 448);
+            this.buttonSendToSkyline.Name = "button1";
+            this.buttonSendToSkyline.Size = new System.Drawing.Size(258, 34);
+            this.buttonSendToSkyline.TabIndex = 1;
+            this.buttonSendToSkyline.Text = "Send to Skyline";
+            this.buttonSendToSkyline.UseVisualStyleBackColor = true;
+            this.buttonSendToSkyline.Click += new System.EventHandler(this.buttonSendToSkylineClick);
+            // 
+            // button2
+            // 
+            this.buttonStoreTransitionList.Location = new System.Drawing.Point(12, 448);
+            this.buttonStoreTransitionList.Name = "button2";
+            this.buttonStoreTransitionList.Size = new System.Drawing.Size(258, 34);
+            this.buttonStoreTransitionList.TabIndex = 2;
+            this.buttonStoreTransitionList.Text = "Store transition list";
+            this.buttonStoreTransitionList.UseVisualStyleBackColor = true;
+            this.buttonStoreTransitionList.Click += new System.EventHandler(this.buttonStoreTransitionListClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBoxHideReplicates.AutoSize = true;
+            this.checkBoxHideReplicates.Location = new System.Drawing.Point(12, 426);
+            this.checkBoxHideReplicates.Name = "checkBox1";
+            this.checkBoxHideReplicates.Size = new System.Drawing.Size(96, 17);
+            this.checkBoxHideReplicates.TabIndex = 3;
+            this.checkBoxHideReplicates.Text = "Only show unique transtions";
+            this.checkBoxHideReplicates.UseVisualStyleBackColor = true;
+            this.checkBoxHideReplicates.CheckedChanged += new System.EventHandler(this.checkBoxCheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBoxCreateSpectralLibrary.AutoSize = true;
+            this.checkBoxCreateSpectralLibrary.Location = new System.Drawing.Point(500, 426);
+            this.checkBoxCreateSpectralLibrary.Name = "checkBox2";
+            this.checkBoxCreateSpectralLibrary.Size = new System.Drawing.Size(96, 17);
+            this.checkBoxCreateSpectralLibrary.TabIndex = 3;
+            this.checkBoxCreateSpectralLibrary.Text = "Create Spectral library";
+            this.checkBoxCreateSpectralLibrary.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.buttonStoreSpectralLibrary.Location = new System.Drawing.Point(809, 448);
+            this.buttonStoreSpectralLibrary.Name = "button3";
+            this.buttonStoreSpectralLibrary.Size = new System.Drawing.Size(258, 34);
+            this.buttonStoreSpectralLibrary.TabIndex = 4;
+            this.buttonStoreSpectralLibrary.Text = "Store spectral library";
+            this.buttonStoreSpectralLibrary.UseVisualStyleBackColor = true;
+            this.buttonStoreSpectralLibrary.Click += new System.EventHandler(this.buttonStoreSpectralLibraryClick);
+            this.buttonStoreSpectralLibrary.Enabled = false;
+            // 
+            // label1
+            // 
+            this.labelNumberOfTransitions.AutoSize = true;
+            this.labelNumberOfTransitions.Location = new System.Drawing.Point(246, 426);
+            this.labelNumberOfTransitions.Name = "label1";
+            this.labelNumberOfTransitions.Size = new System.Drawing.Size(109, 13);
+            this.labelNumberOfTransitions.TabIndex = 5;
+            this.labelNumberOfTransitions.Text = "Number of transitions:";
+            this.labelNumberOfTransitions.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LipidsReview
+            //
+            this.Font = SystemFonts.DialogFont;
+            //this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1079, 494);
+            this.Controls.Add(this.labelNumberOfTransitions);
+            this.Controls.Add(this.buttonStoreSpectralLibrary);
+            this.Controls.Add(this.checkBoxHideReplicates);
+            this.Controls.Add(this.checkBoxCreateSpectralLibrary);
+            this.Controls.Add(this.buttonStoreTransitionList);
+            this.Controls.Add(this.buttonSendToSkyline);
+            this.Controls.Add(this.dataGridViewTransitions);
+            this.Name = "LipidsReview";
+            this.Text = "LipidsReview";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransitions)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            
+            controlElements = new ArrayList(){buttonSendToSkyline, dataGridViewTransitions, buttonStoreTransitionList, checkBoxHideReplicates, checkBoxCreateSpectralLibrary, buttonStoreSpectralLibrary};
+        }
+
+        #endregion
+
+        [NonSerialized]
+        public System.Windows.Forms.DataGridView dataGridViewTransitions;
+        [NonSerialized]
+        public System.Windows.Forms.Button buttonSendToSkyline;
+        [NonSerialized]
+        public Button buttonStoreTransitionList;
+        [NonSerialized]
+        public CheckBox checkBoxHideReplicates;
+        [NonSerialized]
+        public CheckBox checkBoxCreateSpectralLibrary;
+        [NonSerialized]
+        public Button buttonStoreSpectralLibrary;
+        [NonSerialized]
+        public Label labelNumberOfTransitions;
+        public ArrayList controlElements;
+    }
+}
---
+++ LipidCreator/LipidsInterList.cs
@@ -0,0 +1,192 @@
+﻿/*
+MIT License
+
+Copyright (c) 2018 Dominik Kopczynski   -   dominik.kopczynski {at} isas.de
+                   Bing Peng   -   bing.peng {at} isas.de
+                   Nils Hoffmann  -  nils.hoffmann {at} isas.de
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Collections;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace LipidCreator
+{
+    [Serializable]
+    public partial class LipidsInterList : Form
+    {
+        public DataTable transitionList;
+        public DataTable transitionListUnique;
+        public ArrayList replicates;
+        public DataTable currentView;
+        public CreatorGUI creatorGUI;
+        public string[] dataColumns = {};
+
+        public LipidsInterList (CreatorGUI _creatorGUI)
+        {
+            creatorGUI = _creatorGUI;
+            transitionList = creatorGUI.lipidCreator.transitionList;
+            currentView = this.transitionList;
+            replicates = creatorGUI.lipidCreator.replicates;
+            transitionListUnique = creatorGUI.lipidCreator.transitionListUnique;
+            
+            
+            InitializeComponent ();
+            dataGridViewTransitions.DataSource = currentView;
+            buttonSendToSkyline.Enabled = creatorGUI.lipidCreator.openedAsExternal;
+            labelNumberOfTransitions.Text = "Number of transitions: " + currentView.Rows.Count;
+            foreach (DataGridViewColumn dgvc in dataGridViewTransitions.Columns) {
+                dgvc.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            
+            dataGridViewTransitions.Update ();
+            dataGridViewTransitions.Refresh ();
+            
+            if (creatorGUI.lipidCreator.selectedInstrumentForCE.Length > 0)
+            {
+                InstrumentData instrumentData = creatorGUI.lipidCreator.msInstruments[creatorGUI.lipidCreator.selectedInstrumentForCE];
+                buttonStoreSpectralLibrary.Enabled = instrumentData.minCE > 0 && instrumentData.maxCE > 0 && instrumentData.minCE < instrumentData.maxCE;
+            }
+            
+            checkBoxCreateSpectralLibrary.Enabled = creatorGUI.lipidCreator.openedAsExternal && buttonStoreSpectralLibrary.Enabled;
+            
+        }
+        
+        
+        
+        private void gridviewDataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            if (currentView == transitionList)
+            {
+                foreach (int i in replicates) dataGridViewTransitions.Rows[i].DefaultCellStyle.BackColor = Color.Beige;
+            }
+            else dataGridViewTransitions.DefaultCellStyle.BackColor = Color.Empty;
+            foreach (DataGridViewColumn d in dataGridViewTransitions.Columns) d.SortMode = DataGridViewColumnSortMode.NotSortable;
+        }
+        
+        
+
+        public void buttonSendToSkylineClick (Object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            
+            if (checkBoxCreateSpectralLibrary.Checked) {
+                String[] specName = new String[]{""};
+                SpectralName spectralName = new SpectralName (specName);
+                spectralName.Owner = this;
+                spectralName.ShowInTaskbar = false;
+                spectralName.ShowDialog ();
+                spectralName.Dispose ();
+                if (specName [0].Length > 0) {
+                    string blibPath = Application.StartupPath + "\\..\\Temp\\" + specName[0] + ".blib";
+                    creatorGUI.lipidCreator.createBlib (blibPath);
+                    creatorGUI.lipidCreator.sendToSkyline (currentView, specName[0], blibPath);
+                    MessageBox.Show ("Sending transition list and spectral library to Skyline is complete.", "Sending complete");
+                }
+            } else {
+                creatorGUI.lipidCreator.sendToSkyline (currentView, "", "");
+                MessageBox.Show ("Sending transition list to Skyline is complete.", "Sending complete");
+            }
+            this.Enabled = true;
+        }
+
+        
+        
+        
+        private void checkBoxCheckedChanged (object sender, EventArgs e)
+        {
+            if (((CheckBox)sender).Checked)
+            {            
+                currentView = this.transitionListUnique;
+            }
+            else
+            {
+                currentView = this.transitionList;
+            }
+            
+            labelNumberOfTransitions.Text = "Number of transitions: " + currentView.Rows.Count;
+            dataGridViewTransitions.DataSource = currentView;
+            dataGridViewTransitions.Update();
+            dataGridViewTransitions.Refresh();
+        }
+        
+        
+        
+        
+
+        private void buttonStoreTransitionListClick (object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog ();
+            
+            saveFileDialog1.InitialDirectory = "c:\\";
+            saveFileDialog1.Filter = "csv files (*.csv)|*.csv|tsv files (*.tsv)|*.tsv|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog () == DialogResult.OK) {
+                string mode = ".csv";
+                string separator = ",";
+                if(saveFileDialog1.FilterIndex==2) {
+                    mode = ".tsv";
+                    separator = "\t";
+                }
+                DialogResult mbr = MessageBox.Show ("Split polarities into two separate files?", "Storing mode", MessageBoxButtons.YesNo);
+                
+                this.Enabled = false;
+                creatorGUI.lipidCreator.storeTransitionList(separator, (mbr == DialogResult.Yes), Path.GetFullPath (saveFileDialog1.FileName), currentView, mode);
+                this.Enabled = true;
+                MessageBox.Show ("Storing of transition list is complete.", "Storing complete");
+            }
+        }
+        
+        
+        
+        
+
+        private void buttonStoreSpectralLibraryClick (object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog ();
+            
+            saveFileDialog1.InitialDirectory = "c:\\";
+            saveFileDialog1.Filter = "blib files (*.blib)|*.blib|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog () == DialogResult.OK) {
+                this.Enabled = false;
+                try
+                {
+                    creatorGUI.lipidCreator.createBlib(Path.GetFullPath(saveFileDialog1.FileName));
+                    MessageBox.Show("Storing of spectral library is complete.", "Storing complete");
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Problem storing spectral library: " + exception.Message, "Problem storing spectral library");
+                }
+                this.Enabled = true;
+            }
+        }
+    }
+}

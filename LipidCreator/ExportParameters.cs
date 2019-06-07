bdd0e9720908ed782b538fd74ca6f6fb083f9ae9
dominik.kopczynski <dominik.kopczynski@isas.de>
2019-06-07 13:00:19 +0200

    added new dialog ExportParameters

---
+++ LipidCreator/ExportParameters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LipidCreator
+{
+    public partial class ExportParameters : Form
+    {
+        public string[] returnMessage = null;
+        
+        public ExportParameters(string[] _returnMessage)
+        {
+            returnMessage = _returnMessage;
+            InitializeComponent();
+        }
+        
+        protected void mergeClick(object sender, System.EventArgs e)
+        {
+            returnMessage[0] = "merge";
+            Close();        
+        }
+        
+        protected void replaceClick(object sender, System.EventArgs e)
+        {
+            returnMessage[0] = "replace";
+            Close();
+        }
+    }
+}

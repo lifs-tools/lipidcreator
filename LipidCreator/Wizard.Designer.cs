6d9db626992111bb56ea712dadb47140f67b15b5
Nils Hoffmann <nils.hoffmann@isas.de>
2020-03-21 20:29:26 +0100

    Removed old manual, fixed layout issues with hydroxyl labels on Windows.

--- LipidCreator/Wizard.Designer.cs
+++ LipidCreator/Wizard.Designer.cs
@@ -126,7 +126,7 @@ namespace LipidCreator
             String dbText = "No. DB";
             String hydroxylText = "No. Hydroxy";
             int dbLength = 70;
-            int sep = 15;
+            int sep = 20;
             int sepText = 20;
             int faLength = 150;
             string formattingFA = "Comma seperated single entries or intervals. Example formatting: 2, 3, 5-6, 13-20";
